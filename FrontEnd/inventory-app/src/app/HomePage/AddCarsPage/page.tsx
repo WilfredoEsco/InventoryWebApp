d6300c81a5bf7c2177e813d9d16ed797df6b1247
wilfredoEsco <wilfredohernandez2015@gmail.com>
2024-04-05 17:43:05 -0500

    added some routing

---
+++ FrontEnd/inventory-app/src/app/HomePage/AddCarsPage/page.tsx
@@ -0,0 +1,5 @@
+export default function AddCars(){
+    return<h1>Add new Arrivals Page</h1>
+  }
+  
+  
---
+++ FrontEnd/inventory-app/src/app/HomePage/AddPage/page.tsx
@@ -0,0 +1,5 @@
+export default function AddInv(){
+    return<h1>Add Inventory Page</h1>
+  }
+  
+  
